cc4a36c9f9004a2cd2c3f248b556e367bb4c0e9d
AnaeleJoshua <anaelejoshua@gmail.com>
2024-12-26 10:14:55 +0100

    Fix: fixed access token route

--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 const loginRoute = require('./loginRoute')
-const refreshRoute = require('./refreshRoute')
+const refreshRoute = require('./accessTokenRoute')
 const registrationRoute = require('./regRoute')
 
 // Combine individual routes under /auth
 router.use("/login", loginRoute);
 router.use("/register", registrationRoute);
-router.use("/refresh", refreshRoute);
+router.use("/accessToken", refreshRoute);
 
 module.exports = router;
