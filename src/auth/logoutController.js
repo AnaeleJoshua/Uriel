6a5984075c45431982d69cb3d06d8f335dc58688
AnaeleJoshua <anaelejoshua@gmail.com>
2025-06-03 00:46:17 +0100

    fix bug

--- src/auth/logoutController.js
+++ src/auth/logoutController.js
@@ -1,48 +1,45 @@
 const dbInitialization = require("../models/modelInit");
-const getSequelizeInstance = require('../../config/db');
-//requirement
-//clear refresh token from db
-//clear refresh token from cookie
-//
 
-const logOut = async (req,res) => {
-    try{
-        const {User} = await dbInitialization
-        const { cookies: { ['refresh-token']: refreshToken }, user: { userId } } = req;
- //destruct jwt and userId from req object
-        console.log("userId",userId)
-        console.log("refreshToken",refreshToken)
-        if (!refreshToken || !userId) {
-            console.log("No refresh token or userId found in request");
-            return res.status(400).json({status:'failed',
-                'message':'invalid request'
-            })
-        }
-        const user = await User.findByPk(userId);
-        console.log(user)
-        if(user){
-          user.refreshToken = "";
-          await user.save()
-        }
-        // console.log(updateRow)
-        // Set the refresh token as an HTTP-only cookie
-      res.clearCookie("refresh-token", {
-        httpOnly: true,
-        sameSite: "None",
-        secure: true,
+const logOut = async (req, res) => {
+  try {
+    const { User } = dbInitialization;
+    const { cookies: { ['refresh-token']: refreshToken }, user: { userId } } = req;
+
+    console.log("userId", userId);
+    console.log("refreshToken", refreshToken);
+
+    if (!refreshToken || !userId) {
+      console.log("No refresh token or userId found in request");
+      return res.status(400).json({
+        status: 'failed',
+        message: 'Invalid request'
       });
-      return res.status(200).json({status:'success',
-        message:'Logout Successful'
-      })
-
-    }catch(err){
-        console.error(err)
-        res.status(500).json({
-            status:'failed',
-            message:'logout failed'
-        })
     }
-    
-}
 
-module.exports = logOut
+    const user = await User.findByPk(userId);
+    if (user) {
+      user.refreshToken = "";
+      await user.save();
+    }
+
+    res.clearCookie("refresh-token", {
+      httpOnly: true,
+      sameSite: "None",
+      secure: true,
+    });
+
+    return res.status(200).json({
+      status: 'success',
+      message: 'Logout Successful'
+    });
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({
+      status: 'failed',
+      message: 'Logout failed'
+    });
+  }
+};
+
+module.exports = logOut;
