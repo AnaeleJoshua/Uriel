31f2fcf3d581cba859066d53e3c94898dcf0ff27
AnaeleJoshua <anaelejoshua@gmail.com>
2025-06-21 06:44:09 +0000

    add blacklisting to used token

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -24,7 +24,7 @@ module.exports = {
       }
 
       const organisations = await Organisation.findAll({
-        attributes: ["name"],
+        attributes: ["orgName"],
         include: {
           model: User,
           where: { userId: id },
